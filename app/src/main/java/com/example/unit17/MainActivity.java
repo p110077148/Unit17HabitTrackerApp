70d7805cb9732d65b2caddd47fe09d98b77f27ba
falc174 <p110077148@student.peterborough.ac.uk>
2025-05-21 22:57:52 +0100

    nearly done

--- app/src/main/java/com/example/unit17/MainActivity.java
+++ app/src/main/java/com/example/unit17/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Force full-screen layout so we control the status bar background
+        // Force full-screen layout
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         );
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         // Connect toolbar
-        setSupportActionBar(findViewById(R.id.toolbar));
+        setSupportActionBar(binding.toolbar);
 
         // black top and bottom
         getWindow().setStatusBarColor(Color.BLACK);
